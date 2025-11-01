af32ef98b198f25fef82283d4fab58dc10ccbf45
Sabayas <sabayasmin345@gmail.com>
2025-11-01 20:02:13 +0530

    Modified

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const quoteText = document.querySelector('.qoute');
+authorName = document.querySelector(".author .name")
 const button = document.querySelector('.btn')
 
 // random quote function
 
 function randomQuote(){
-    fetch("https:")
+    fetch("https://dummyjson.com/quotes").then(res => res.json()).then(result => {
+        console.log(result)
+        quoteText.innerText = result.qoutes[0];
+        authorName.innerText = result.author
+    });
+  
 }
 
 
 button.addEventListener('click', randomQuote);
+
+randomQuote();
